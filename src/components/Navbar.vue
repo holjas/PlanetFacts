b4181480944ba7353bebb2703c1292ba0bfa9815
Holly J <63270908+holjas@users.noreply.github.com>
2021-12-31 19:49:08 -0500

    clean up and readme images

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,45 +1,4 @@
 <template>
-  <!-- <nav class="border-b border-gray-600"> -->
-  <!-- <div
-      class="
-        flex
-        xs:flex-wrap xs:flex-col
-        lg:flex-nowrap lg:flex-row
-        items-center
-        p-5
-        uppercase
-        wrapper
-      "
-    >
-      
-      <div class="lg:w-1/4 xs:w-full xs:text-center xs:pb-3">
-        <router-link :to="{ name: 'Home' }">
-          <h2>the planets</h2>
-        </router-link>
-      </div>
-      
-      <div class="lg:w-screen">
-        <ul class="text-right">
-          <li
-            v-for="planet in planets"
-            :key="planet.name"
-            class="text-xs font-bold px-5 text-gray-400"
-          >
-            <router-link
-              :to="{
-                name: 'PlanetDetails',
-                params: { id: planet.name },
-              }"
-            >
-              {{ planet.name }}
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </div> 
-    </nav>-->
-  <!--  -->
-  <!--  -->
   <nav
     class="
       flex
--- src/views/PlanetDetails.vue
+++ src/views/PlanetDetails.vue
@@ -1,3 +1,9 @@
+Skip to content Search or jump to… Pull requests Issues Marketplace Explore
+@holjas holjas / PlanetFacts Public Code Issues Pull requests Actions Projects
+Wiki Security Insights Settings PlanetFacts/src/views/PlanetDetails.vue @holjas
+holjas side bar buttons removed on small, box details responsive Latest commit
+da08285 on Aug 15 History 1 contributor 208 lines (201 sloc) 5.85 KB
+
 <template>
   <section :key="id + activeClass" class="wrapper">
     <div class="flex h-auto xs:flex-wrap xl:flex-nowrap">
@@ -13,7 +19,7 @@
 
       <!-- side bar details -->
       <div
-        class="flex xs:flex-col-reverse xs:p-6 xs:mt-6 md:flex-row xl:w-6/12 xl:p-3"
+        class="flex xs:flex-col-reverse xs:p-6 xs:mt-6 md:flex-row xl:flex-col xl:w-6/12 xl:p-3"
       >
         <div class="xs:w-full md:w-8/12 xl:w-full">
           <h1 class="mb-6">
@@ -32,22 +38,19 @@
           </p>
         </div>
 
-        <!-- buttons -->
         <div
           class="
        
             xs:grid xs:grid-cols-3 xs:gap-2 xs:place-content-evenly
-
-            md:flex
-         
-           
-            md:mx-6
-            
+                 
+            md:w-4/12
+            md:ml-6
+            md:xs:place-content-start
             xl:w-full
             xl:mx-0
- md:w-4/12
- md:justify-center
-  md:flex-col
+            md:flex-col
+            md:flex
+            
           "
         >
           <button
@@ -55,14 +58,14 @@
             name="overview"
             :class="[activeClass === 'overview' ? 'btn-active' : '']"
           >
-            <span class="xs:hidden md:inline ">01</span>overview
+            <span class="xs:hidden md:inline">01</span>overview
           </button>
           <button
             @click="toggleDetailView"
             name="structure"
             :class="[activeClass === 'structure' ? 'btn-active' : '']"
           >
-            <span class="xs:hidden md:inline ">02</span>internal structure
+            <span class="xs:hidden md:inline">02</span>internal structure
           </button>
           <button
             @click="toggleDetailView"
@@ -158,7 +161,6 @@ export default {
   uppercase text-xs font-bold
   md:w-3/12 md:text-left md:flex-col md:justify-start;
 }
-
 button {
   @apply text-white uppercase text-xs  font-bold leading-7 py-3 px-4 my-2 border border-gray-600 rounded-sm
   hover:bg-purple-700 transition duration-300
@@ -173,12 +175,6 @@ button {
 button span {
   @apply text-gray-400 font-normal pl-3 pr-6;
 }
-
-/* responsive colors
-  xs:bg-yellow-200
-sm:bg-red-200
-        md:bg-blue-200
-        lg:bg-green-200
-        xl:bg-orange-200
-         */
 </style>
+© 2021 GitHub, Inc. Terms Privacy Security Status Docs Contact GitHub Pricing
+API Training Blog About
